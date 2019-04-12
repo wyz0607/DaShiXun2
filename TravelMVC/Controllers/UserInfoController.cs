5d1c01cb46dedb1a8f64f2816b92629b4d646848
纪伟 薛 <406955883@qq.com>
2019-04-12 10:21:31 +0800

    xjw

--- TravelMVC/Controllers/UserInfoController.cs
+++ TravelMVC/Controllers/UserInfoController.cs
@@ -3,19 +3,21 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using TravelMVC.Models;
+using Newtonsoft.Json;
 
 namespace TravelMVC.Controllers
 {
     public class UserInfoController : Controller
     {
-        // GET: UserInfo
+        #region 登录
         [HttpGet]
         public ActionResult Login()
         {
             return View();
         }
         [HttpPost]
-        public void Login(string UserName="",string UserPwd="")
+        public void Login(string UserName = "", string UserPwd = "")
         {
             if (UserName == "")
             {
@@ -27,8 +29,38 @@ namespace TravelMVC.Controllers
             }
             else
             {
-                string jsonResult = HttpClientHelper.Send("get", "");
+                string jsonResult = HttpClientHelper.Send("get", $"api/UserInfoApi/GetLoginUser?UserName={UserName}&UserPwd={UserPwd}");
+                UserInfo user = JsonConvert.DeserializeObject<UserInfo>(jsonResult);
+                if (user == null)
+                {
+                    Response.Write("<script>alert('用户名或密码有误,请重新登录');location.href='/UserInfo/Login';</script>");
+                }
+                else
+                {
+                    Session["User"] = user;
+                    Response.Write("<script>location.href='/UserInfo/Login';</script>");
+                }
             }
         }
+        #endregion
+        #region UserInfo列表、添加管理员
+        //获取所有下拉列表的数据方法
+        public string BindSelect(int Id = 0)
+        {
+            string jsonResult = HttpClientHelper.Send("get", $"api/UserInfoApi/ShowRegion/{Id}");
+            List<Region> list = JsonConvert.DeserializeObject<List<Region>>(jsonResult);
+            return JsonConvert.SerializeObject(list);
+        }
+        [HttpGet]
+        public ActionResult AddUser()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult AddUser(UserInfo user)
+        {
+            return View();
+        }
+        #endregion
     }
 }
--- TravelMVC/Models/UserInfo.cs
+++ TravelMVC/Models/UserInfo.cs
@@ -8,17 +8,22 @@ namespace TravelMVC.Models
     public class UserInfo
     {
         public int UserID { get; set; }
-        public string  UserName { get; set; }
-        public string UserPhone { get; set; }
-        public string UserPwd { get; set; }
-        public string UserSex { get; set; }
-        public string UserBirth { get; set; }
+        public string  UserName { get; set; }//用户名
+        public string UserPhone { get; set; }//手机号
+        public string UserPwd { get; set; }//密码
+        public string UserSex { get; set; }//性别
+        public string UserBirth { get; set; }//出生日期
         public int User_Loc_Prov { get; set; }
         public int User_Loc_City { get; set; }
         public int User_Loc_Coun { get; set; }
-        public string User_DocumentType { get; set; }
-        public string User_IDNumber { get; set; }
-        public string Role { get; set; }
-     
+        public string User_DocumentType { get; set; }//证件类型
+        public string User_IDNumber { get; set; }//证件号
+        public string User_Role { get; set; }//角色
+
+
+        //外键用到字段
+        public string RName { get; set; }      //三级联动时显示名称
+        public string CityName { get; set; }
+        public string CounName { get; set; }
     }
 }
